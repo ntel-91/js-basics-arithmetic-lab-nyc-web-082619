a1afe7b207b62f11e89c81c16d49d4cac18fea21
Nick Telenson <ntelenson@gmail.com>
2019-10-04 16:55:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here
+let var1 = 2;
+let var2 = 31;
+let multiply = var1 * var2;
+
+let random = Math.ceil(Math.random()*10);
+
+let mod = 9 % 5;
+
+let max = Math.max(1,3,20,15);
